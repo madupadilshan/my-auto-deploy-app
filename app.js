30f281924176b5433ea6aa3fe078619f2ba66d7a
Madupa Dilshan <madupadilshan111@gmail.com>
2025-07-04 22:46:17 +0530

    change app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Congratulations!  </h1>');
+  res.send('<h1>Congratulations!  its work full CI/CD pipeline </h1>');
 });
 
 app.listen(PORT, () => {
